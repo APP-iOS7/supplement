7a8fd8eae2164eec6de9b7836237b494de9dcdb8
Tom Bum Su Choi <qjatn0545@naver.com>
2025-03-03 01:03:52 +0900

    GeminiService 클래스 추가 및 추천 영양제 API 호출 구현

---
+++ lib/services/gemini_service.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:supplementary_app/secrets.dart';
+
+//파라메터와 리턴 수정해야함. -> 리런은 List<String>으로.
+
+class GeminiService {
+  Future<void> getRecommendSupplement() async {
+    final apiKey = Secrets.geminiApi;
+    final url =
+        'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=$apiKey';
+
+    final headers = {'Content-Type': 'application/json'};
+    final body = jsonEncode({
+      "contents": [
+        {
+          "parts": [
+            {
+              "text": '''
+나는 31세 여성이며, 수면 장애를 개선하고 싶어. 
+      이를 위해 도움이 되는 영양제 제품 10개를 추천해줘. 
+      브랜드명을 포함한 정확한 제품명을 제공해줘. 
+      반드시 JSON 형식으로 응답해야 하며, 줄바꿈 없이 한 줄 JSON으로 반환해. 
+      JSON 코드 블록( ```json ... ``` )을 사용하지 마. 
+      추가적인 설명 없이 순수한 JSON 데이터만 반환해. 
+      정확한 JSON 형식은 다음과 같아:
+      
+      {
+        "recommendations": [
+          {"name": ""},
+          {"name": ""},
+          {"name": ""},
+          {"name": ""},
+          {"name": ""},
+          {"name": ""},
+          {"name": ""},
+          {"name": ""},
+          {"name": ""},
+          {"name": ""}
+        ]
+      }
+      
+      줄바꿈 없이, 한 줄 JSON 데이터만 응답해.
+      ''',
+            },
+          ],
+        },
+      ],
+    });
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body);
+      print(jsonResponse); // JSON 데이터 출력
+    } else {
+      print('API 호출 실패: ${response.statusCode}');
+      print(response.body);
+    }
+  }
+}
