a0c79283a588253057324512d066aa995a7b519f
Tom Bum Su Choi <qjatn0545@naver.com>
2025-03-02 17:40:03 +0900

    홈 화면 및 마이페이지 화면 추가, 메인 화면에 하단 내비게이션 바 구현

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:supplementary_app/screens/home/home_screen.dart';
+import 'package:supplementary_app/screens/mypage/mypage_screen.dart';
+import 'package:supplementary_app/screens/search/search_screen.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [HomeScreen(), SearchScreen(), MypageScreen()];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.receipt), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.pin_drop), label: 'My Page'),
+        ],
+      ),
+    );
   }
 }
