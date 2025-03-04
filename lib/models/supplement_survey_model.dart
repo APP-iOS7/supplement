678f3d4a42d391a1f3578ec8dfea23aada459e2b
Tom Bum Su Choi <qjatn0545@naver.com>
2025-03-04 16:37:05 +0900

    모델 클래스 수정 및 추가: GeminiAnswerModel을 AnswerModel로 변경하고, GovSupplementModel 삭제, SupplementSurveyModel 추가. 서비스 및 프로바이더 수정.

---
+++ lib/models/supplement_survey_model.dart
@@ -0,0 +1,10 @@
+class SupplementSurveyModel {
+  final List<String> goal = [];
+  final bool isSmoker = false;
+  final bool isDerinker = false;
+  final List<String> alergy = [];
+  final List<String> prescribedDrugs = [];
+  final int exerciseFrequencyPerWeek = 0;
+
+  SupplementSurveyModel();
+}
---
+++ lib/providers/supplement_survey_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:supplementary_app/models/supplement_survey_model.dart';
+
+class SupplementSurveyProvider with ChangeNotifier {
+  var survay = SupplementSurveyModel();
+}
