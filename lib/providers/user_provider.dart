c1c6eb0da89447dafece8cc7a6a880acbe3d584f
Tom Bum Su Choi <qjatn0545@naver.com>
2025-03-05 16:41:16 +0900

    UserModel에 생성일, 로그아웃 여부, 마지막 로그인/로그아웃 시간 및 마지막 업데이트 시간 필드 추가; UserProvider에서 사용자 상태 관리 기능 구현

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:supplementary_app/models/user_model.dart';
 
+class UserProvider with ChangeNotifier {
+  UserModel? _user;
+
+  UserModel? get user => _user;
+
+  void setUser(UserModel? user) {
+    _user = user;
+    notifyListeners();
+  }
+
+  void clearUser() {
+    _user = null;
+    notifyListeners();
+  }
+}
