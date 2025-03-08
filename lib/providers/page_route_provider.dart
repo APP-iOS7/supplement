c66348dc512d86468da26d109bef9158b6726d07
Tom Bum Su Choi <qjatn0545@naver.com>
2025-03-08 19:53:09 +0900

    feat: 페이지 라우팅 로직 개선 - 현재 인덱스 관리 및 네비게이션 메서드 추가

--- lib/providers/page_route_provider.dart
+++ lib/providers/page_route_provider.dart
@@ -1,5 +1,24 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class PageRouteProvider with ChangeNotifier {
-  final int _healthRecommendPageIndex = 0;
+  int _currentIndex = 0;
+
+  int get currentIndex => _currentIndex;
+
+  void navigateToMain() {
+    _currentIndex = 0;
+    notifyListeners();
+  }
+
+  void navigateToHealthCheck() {
+    _currentIndex = 1;
+    notifyListeners();
+  }
+
+  void goBack() {
+    if (_currentIndex > 0) {
+      _currentIndex--;
+      notifyListeners();
+    }
+  }
 }
